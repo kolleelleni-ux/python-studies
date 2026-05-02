ba3e72e073e806a4bb3ac4419f0fefe76733721c
Danielle Tsuchida <kolle.elleni@gmail.com>
2026-05-02 15:14:10 -0300

    Add initial Python script with various examples

---
+++ ex1.py
@@ -0,0 +1,153 @@
+############################################################################################################################
+###################################################### LISTA 01 ############################################################
+############################################################################################################################
+# 1-A
+print('Hello World!')
+
+############################################################################################################################
+# 1-B
+name = "Danielle Keiko Tsuchida"
+
+print(f"Meu nome e {name}")
+
+############################################################################################################################
+# 1-C
+name = 'Danielle'
+subject = 'Introducao a programacao de computadores'
+
+print(f"Materia: {subject}")
+print(f"Nome: {name}")
+print("Estou aprendendo Python!")
+
+############################################################################################################################
+# 1-D
+name = "Danielle"
+age = int ("25")
+city = "Maringa"
+major = "Analise e Desenvolvimento de Sistemas"
+
+print(f"Meu nome e {name}")
+print(f"Tenho {age} anos")
+print(f"Moro em {city}")
+print(f"Estou cursando {major}")
+
+############################################################################################################################
+# 2-A
+mname = "Babadook"
+myear = int ("2014")
+mrate = int ("7")
+
+print(f"{mname}")
+print(f"{myear}")
+print(f"{mrate}")
+
+###########################################################################################################################
+# 2-B
+product = "Resident Evil Requiem PS5"
+price = float ("324.57")
+stock = int ("1000")
+
+print(f"{product}")
+print(f"{price}")
+print(f"{stock}")
+
+###########################################################################################################################
+# 2-C
+name = "George"
+grade = "1-A"
+score = float ("9.3")
+
+print(f"O Aluno {name} da turma {grade} pontuou {score} na ultima prova")
+
+###########################################################################################################################
+# 2-D
+destino = "Florianopolis"
+dias = int ("12")
+passagem = float ("263.45")
+transporte = "Onibus"
+
+print(f"{destino}")
+print(f"{dias}")
+print(f"{passagem}")
+print(f"{transporte}")
+
+###########################################################################################################################
+# 3-A
+nome = str ("Python")
+ano = int ("2026")
+pi = float ("3.14")
+resposta = bool ("False")
+
+print(type(nome))
+print(type(ano))
+print(type(pi))
+print(type(resposta))
+
+###########################################################################################################################
+# 3-B
+nome = str ("George")
+idade = int ("24")
+altura = float ("1.71")
+resposta = bool ("True")
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(resposta))
+
+###########################################################################################################################
+# 3-C
+primeiro = str("123")
+segundo = int ("123")
+terceiro = float ("12.3")
+quarto = bool ("True")
+
+print(f"{primeiro} {type(primeiro)}")
+print(f"{segundo} {type(segundo)}")
+print(f"{terceiro} {type(terceiro)}")
+print(f"{quarto} {type(quarto)}")
+
+###########################################################################################################################
+# 3-D
+cidade = "Tokyo"
+habitantes = int ("37000000")
+temperatura = "15ºC"
+chovendo = bool ("True")
+
+print(type(cidade))
+print(type(habitantes))
+print(type(temperatura))
+print(type(chovendo))
+
+###########################################################################################################################
+# 4-A
+idade = "18"
+idade_texto = int(idade)
+
+print(f"{idade} {type(idade_texto)}")
+
+###########################################################################################################################
+# 4-B
+preco = "59.90"
+preco_texto = float(preco)
+
+print(f"{preco} {type(preco_texto)}")
+
+###########################################################################################################################
+# 4-C
+nint = 67
+nint_texto = str(nint)
+
+print(f"{nint} {type(nint_texto)}")
+
+###########################################################################################################################
+# 4-D
+
+num1 = "45"
+num2 = "7.8"
+
+num1_texto = int(num1)
+num2_texto = float(num2)
+
+print(f"{num1} {type(num1_texto)}")
+print(f"{num2} {type(num2_texto)}")
